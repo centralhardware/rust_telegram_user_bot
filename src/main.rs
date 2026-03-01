a2ec7166b985d603353067ea835ecce0693beadd
alex <alex@centralhardware.me>
2026-03-01 22:49:52 +0800

    Revert "adjust logger"
    This reverts commit 20ea76326b43d678777756f3f32936177bf3a22d.

--- src/main.rs
+++ src/main.rs
@@ -19,15 +19,7 @@ async fn main() -> Result<()> {
         .format(move |buf, record| {
             use std::io::Write;
             let now = chrono::Utc::now().with_timezone(&tz);
-            writeln!(
-                buf,
-                "[{}] [{}] [{}:{}] {}",
-                now.format("%H:%M:%S"),
-                record.level(),
-                record.file().unwrap_or("unknown"),
-                record.line().unwrap_or(0),
-                record.args()
-            )
+            writeln!(buf, "[{}] {}", now.format("%H:%M:%S"), record.args())
         })
         .init();
     std::panic::set_hook(Box::new(|info| {
